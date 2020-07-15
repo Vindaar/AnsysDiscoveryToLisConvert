41a763a03ce9c516ae079fc95e624321355d1410
Vindaar <basti90@gmail.com>
2020-07-15 19:57:37 +0200

    add writing of NLIST.lis files

--- ansysDiscovConvert.nim
+++ ansysDiscovConvert.nim
@@ -1,4 +1,4 @@
-import strscans, strutils, streams, os
+import strscans, strutils, streams, os, sequtils, algorithm
 import cligen
 
 ## The node information by default should be contained in the
@@ -9,6 +9,15 @@ type
   Node = object
     id: int
     pos: tuple[x, y, z: float]
+
+func `$`(n: Node): string =
+  result = newStringOfCap(80)
+  result.add align($n.id, 20)
+  result.add align(formatFloat(n.pos.x, precision = 11), 20)
+  result.add align(formatFloat(n.pos.y, precision = 11), 20)
+  result.add align(formatFloat(n.pos.z, precision = 11), 20)
+
+func `<`(n, m: Node): bool = n.id < m.id
 
 proc parseNodes(path: string): seq[Node] =
   const blkStr = "nblock,$i,,$i"
@@ -27,7 +36,6 @@ proc parseNodes(path: string): seq[Node] =
   if not buf.scanf(blkStr, nBlk, nNodes):
     raise newException(IOError, "Not a valid mesh file! `nblock` header could " &
       "not be matched.")
-  echo nNodes
   doAssert f.readLine(buf)
   assert buf.startsWith("(")
   var
@@ -41,9 +49,39 @@ proc parseNodes(path: string): seq[Node] =
     inc idx
   f.close()
 
-proc main(path: string) =
-  # parse the nodes
-  let nodes = parseNodes(path)
+proc writeNLIST(nodes: seq[Node], outpath: string) =
+  ## writes the nodes to the `NLIST.lis` file as expected by garfield
+  ##
+  ## file layout:
+  ## - header consists of empty line, one LIST ... line
+  ## - tables: empty line, table header (NODE, X, Y, Z), 20 nodes
+  const numPerTab = 20
+  const header = "\n LIST ALL SELECTED NODES.   DSYS=      0"
+  # TODO: find out if it matters whether there is more / less space than the
+  # weird `NLIST.lis` (3, 4, 9, 9) spaces...
+  let tabHeader = ["NODE", "X", "Y", "Z"].mapIt(it.align(20)).join()
+  template writeTab(f, n, num: untyped): untyped =
+    f.write("\n\n")
+    f.write(tabHeader & "\n")
+    for idx in 0 ..< num:
+      f.write($n[idx] & "\n")
+
+  var f = open(outpath / "NLIST.lis", fmWrite)
+  f.write(header & "\n")
+  let
+    nFull = nodes.len div numPerTab
+    nRest = nodes.len mod numPerTab
+  for i in 0 ..< nFull:
+    writeTab(f, nodes, numPerTab)
+  # write the remaining
+  if nRest > 0:
+    writeTab(f, nodes, nRest)
+
+proc main(path: string, outpath = ".") =
+  var nodes = parseNodes(path)
+  echo "parsing of ", nodes.len, " nodes done"
+  nodes.sort() # sort by id
+  writeNLIST(nodes, outpath)
 
 
 when isMainModule:
